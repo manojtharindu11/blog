18c9dab486ea9e94a4eac3034ba7b13e1c6ccac7
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-05 00:54:16 +0530

    Make minimal api to control base api

---
+++ src/API/Controllers/WhetherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhetherController : ControllerBase
+    {
+        [HttpGet("/weatherforecast")]
+        public WeatherForecast[] getWeatherForecasts()
+        {
+            var summaries = new[]
+            {
+             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                            (
+                                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                                Random.Shared.Next(-20, 55),
+                                summaries[Random.Shared.Next(summaries.Length)]
+                            ))
+                            .ToArray();
+            return forecast;
+        }
+
+        public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+        {
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
