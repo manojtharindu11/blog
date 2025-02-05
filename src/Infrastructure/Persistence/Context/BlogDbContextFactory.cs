9afe05d6476e990c0f31e50539f6db70246e0bf5
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-05 13:10:25 +0530

    Coreect the Persistence name

---
+++ src/Infrastructure/Persistence/Context/BlogDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.Context
+{
+    public class BlogDbContextFactory : IDesignTimeDbContextFactory<BlogDbContext>
+    {
+        public BlogDbContext CreateDbContext(string[] args)
+        {
+            var optionsbuilder = new DbContextOptionsBuilder<BlogDbContext>();
+            optionsbuilder.UseSqlServer("Data Source=localhost;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            return new BlogDbContext(optionsbuilder.Options);
+        }
+    }
+}
