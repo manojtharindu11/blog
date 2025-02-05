a0e579ab4a0f05fdded47b30d210fdc13873415b
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-05 12:24:35 +0530

    Create UserRole configurations

---
+++ src/Infrastructure/Persistance/Configuration/UserRoleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.ToTable("UserRole");
+
+            builder.HasKey(x => new { x.UserId, x.RoleId });
+
+            builder.HasOne(u => u.User).WithMany(ur => ur.UserRoles).HasForeignKey(u => u.UserId);
+            builder.HasOne(r => r.Role).WithMany(ur => ur.UserRoles).HasForeignKey(r => r.RoleId);
+        }
+    }
+}
