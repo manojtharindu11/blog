40d92894a396fd27c5a63e3644f9b0e3dfa5ce48
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-05 18:45:34 +0530

    Implement the generic repository

--- src/Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ src/Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace Infrastructure.Persistence.Repositories
 {
     public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
     {
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
