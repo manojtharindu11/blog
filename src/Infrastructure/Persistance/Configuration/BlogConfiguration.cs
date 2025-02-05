e5fccaa6ff6523d9a12a1785064d41c4413f23c2
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-05 12:01:01 +0530

    Create Blog entity configurations

---
+++ src/Infrastructure/Persistance/Configuration/BlogConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class BlogConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Title).IsRequired().HasMaxLength(250);
+            builder.Property(c => c.Content).IsRequired();
+            builder.Property(i => i.Image).IsRequired();
+            builder.Property(ca => ca.CreatedAt).IsRequired().HasDefaultValue(DateTime.Now);
+            builder.Property(ua => ua.UpdateAt).IsRequired().HasDefaultValue(DateTime.Now);
+            builder.HasOne(u => u.User).WithMany(b => b.Blogs).HasForeignKey(ui => ui.UserId).OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
