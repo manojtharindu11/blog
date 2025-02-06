c02a4c1385a004a7d0f0be87c9c22388231af62e
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-06 17:37:59 +0530

    Create IUserRepository

---
+++ src/Domain/Interface/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IUserRepository: IGenericRepository<User>
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<List<string>> GetUserRoleByEmailAsync(string email);
+    }
+}
