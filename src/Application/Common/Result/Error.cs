84ef7941a6583c2a858389cb5a86f2cb2b3ebaa3
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-02-07 14:05:13 +0530

    Create error result

---
+++ src/Application/Common/Result/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Result
+{
+    public sealed record Error(string Code, string Message)
+    {
+        internal static Error None => new("None", string.Empty);
+    }
+}
---
+++ src/Application/Common/Result/TResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Result
+{
+    public class Result<TValue>: Result
+    {
+        private readonly TValue _value;
+
+        protected internal Result(TValue value,bool isSuccess,Error error): base(isSuccess, error)
+        {
+               _value = value;
+        }
+
+        public TValue Value => IsSuccess ? _value : throw new InvalidOperationException("No value for failure result");
+    }
+}
