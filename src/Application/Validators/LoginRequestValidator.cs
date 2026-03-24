a8ee8abdacd3173ce01079a0e8095751f74f0604
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-03-24 10:00:43 +0530

    chore: create login request validator

---
+++ src/Application/Validators/LoginRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.Models.Request;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators
+{
+    public class LoginRequestValidator: AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Email is not valid");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Pasword must be at least 6 characters long");
+        }
+    }
+}
