45c106dc2be28a6ecfdd438a10a15e044f108634
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-03-25 12:02:34 +0530

    feat: add UsersDTO

---
+++ src/Application/DTOs/UsersDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public record UsersDTO(int Id, string Email, string Username, List<string> Roles);
+}
