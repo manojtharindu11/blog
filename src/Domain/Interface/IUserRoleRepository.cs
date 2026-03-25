8f637a29c0d0efbf4af7c08107da6d8887085ba5
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-03-25 17:04:19 +0530

    feat: add IUserRoleRepository interface

---
+++ src/Domain/Interface/IUserRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IUserRoleRepository
+    {
+        Task<bool> AddAsync(int userId, int roleId);
+        Task<bool> RemoveAsync(int userId, int roleId);
+        Task<bool> HasRoleAsync(int userId);
+    }
+}
