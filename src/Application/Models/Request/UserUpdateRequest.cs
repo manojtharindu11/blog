0fbfccc3bedd1de18e94ab303b2a6ec5ed139381
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-03-25 12:38:29 +0530

    feat: add AssignRoleRequest and UserUpdteRequest records

--- src/Application/Models/Request/UserUpdateRequest.cs
+++ src/Application/Models/Request/UserUpdateRequest.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Application.Models.Request
 {
-    public record UserUpdateRequest(int Id, string Username, string Email);
+    public record UserUpdateRequest(int Id, string UserName, string Email);
+
 }
