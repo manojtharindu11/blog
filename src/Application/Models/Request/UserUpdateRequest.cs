d1069940f50cb048caff59f6ee6f60534b7c097c
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-03-25 12:22:38 +0530

    feat: add UserUpdateRequest model

---
+++ src/Application/Models/Request/UserUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Request
+{
+    public record UserUpdateRequest(int Id, string Username, string Email);
+}
