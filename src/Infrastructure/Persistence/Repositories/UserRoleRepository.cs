600685c51390998a2bf74cb7d9e1d6c99a5335a9
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-03-25 17:06:28 +0530

    reafac: update the swagger doc title

---
+++ src/Infrastructure/Persistence/Repositories/UserRoleRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Interface;
+using Infrastructure.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class UserRoleRepository(BlogDbContext blogDbContext) : IUserRoleRepository
+    {
+        public Task<bool> AddAsync(int userId, int roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasRoleAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(int userId, int roleId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
